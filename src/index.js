8da57c9da15295f667eb3c1caa1de40050f4a13f
LuizFelixDev <luizhenriquefelix138@gmail.com>
2025-08-31 14:03:33 -0300

    Melhorando a aparência do terminal

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import prompt from "prompt";
 import {mainPrompt} from "./prompt/prompt-main.js"
+import chalk from "chalk";
 
 async function main() {
     prompt.get(mainPrompt, async(err, choose) => {
         if(choose.select == 1){
-            console.log("Escolheu o QRcode");
+            console.log(chalk.blue("Escolheu o QRcode"));
         }
         if(choose.select == 2){
-            console.log("Escolheu o Password");
+            console.log(chalk.blue("Escolheu o Password"));
         };
     });
 }
--- src/prompt/prompt-main.js
+++ src/prompt/prompt-main.js
@@ -1,9 +1,11 @@
+import chalk from "chalk"
+
 const mainPrompt = [
     {
         name: "select",
-        description: "Escolha a ferramenta (1 - QRCODE OU 2 - PASSWORD)",
-        patern: /^[1-2]+$/,
-        message: "Esolha apenas entre 1 e 2",
+        description: chalk.yellow.bold("Escolha a ferramenta (1 - QRCODE OU 2 - PASSWORD)"),
+        pattern: /^[1-2]+$/,
+        message: chalk.green.italic("Esolha apenas entre 1 e 2"),
         required: true,
     }
 ]
